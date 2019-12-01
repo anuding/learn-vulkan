781eb4ee9b68a83c11514fa6945acc52ef7feb1f
anuding <anuding@126.com>
2019-12-01 17:39:17 +0800

    test

--- src/Application.cpp
+++ src/Application.cpp
@@ -56,7 +56,7 @@ namespace Engine::RenderCore {
         glfwDestroyWindow(_window);
         glfwTerminate();
     }
-
+//test
     void Application::createInstance() {
         if (enableValidationLayers)
             checkValidationLayerSupport();
