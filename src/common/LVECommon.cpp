0f529b3a1d91488eaad69f9f5649971196024f49
anuding <anuding@126.com>
2020-03-20 00:21:57 +0800

    add glfwinit to make sure our app meets vulkan

--- src/common/LVECommon.cpp
+++ src/common/LVECommon.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 void sayHi() {
+	glfwInit();
 	if(glfwVulkanSupported())
 		printf("Hi vulkan!\n");
 	else
--- src/common/LVECommon.h
+++ src/common/LVECommon.h
@@ -1,4 +1,6 @@
 #define GLFW_INCLUDE_VULKAN
+#define GLM_FORCE_RADIANS
+
 #include <vulkan/vulkan.h>
 #include <GLFW/glfw3.h>
 
