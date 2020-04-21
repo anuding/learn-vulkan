cd823614f017139dd6e6fc5ee0f9fbbbfa8b2bdf
anuding <anuding@126.com>
2020-04-21 23:35:48 +0800

    combine Buffer and Image

--- src/render_core/Texture.cpp
+++ src/render_core/Texture.cpp
@@ -1,1 +1,40 @@
+#pragma once
 #include "Texture.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#include "Buffer.h"
+#include "BufferFactory.h"
+#include <stdexcept>
+#include "Application.h"
+
+namespace Engine::RenderCore {
+	Texture::~Texture() {
+
+	}
+	void Texture::init(Application* app, char* path)
+	{
+		this->app = app;
+		this->path = path;
+		createTexture();
+	}
+	void Texture::createTexture()
+	{
+		//Buffer side
+		unsigned char* data;
+		uint64_t dataSize;
+		loadRawImage(path, data, dataSize);
+		auto stagingBuffer = BufferFactory::createStagingBuffer(this->app, dataSize);
+		stagingBuffer->updateBufferMemory(data, dataSize);
+
+		//Image side
+	}
+	
+	void Texture::loadRawImage(const char* path, unsigned char* & imageData, uint64_t& dataSize)
+	{
+		int width, height, channels;
+		imageData = stbi_load(path, &width, &height, &channels, STBI_rgb_alpha);
+		dataSize = width * height * 4;
+		if (!imageData)
+			throw std::runtime_error("failed to load raw image from");
+	}
+}
--- src/render_core/Texture.h
+++ src/render_core/Texture.h
@@ -1,20 +1,23 @@
 #pragma once
 #include <vulkan/vulkan.h>
-#include <GLFW/glfw3.h>
+#include "Image.h"
+#include "Buffer.h"
 
 namespace Engine::RenderCore {
 	class Application;
-
 	class Texture {
 	public:
 		Texture() = default;
 		~Texture();
-		void init(Application* app);
+		void init(Application* app,  char* path);
 
 	private:
-		VkImage image;
-		VkImageView imageView;
-		VkDeviceMemory imageMemory;
+		char* path;
+		Application* app;
+		Image image;
+		Buffer stagingBuffer;
 		void createTexture();
+		void loadRawImage(const char* path, unsigned char*& imageData, uint64_t& dataSize);
+
 	};
 }
