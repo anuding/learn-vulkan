9861e101280a93168c6fed5b3e3e8a28bf81d30d
anuding <anuding@126.com>
2020-04-22 11:30:12 +0800

    add window creation checker

--- src/render_core/Window.cpp
+++ src/render_core/Window.cpp
@@ -5,10 +5,13 @@ namespace Engine::RenderCore {
 	void Window::init(Application* app)
 	{
 		this->app = app;
-		auto isInit = glfwInit();
+		if(!glfwInit())
+			throw std::runtime_error("failed to init GLFW!");
 		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 		glfwWindowHint(GLFW_CLIENT_API, GLFW_FALSE);
 		this->get() = glfwCreateWindow(this->app->WINDOW_WIDTH, this->app->WINDOW_HEIGHT, this->app->WINDOW_NAME, nullptr, nullptr);
+		if(!this->get())
+			throw std::runtime_error("failed to create window!");
 	}
 
 	Window::~Window()
